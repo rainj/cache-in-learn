7b643b4616f73d3b7ff2ab5e383ae87f4fe8b4be
刘静 <liujing@liujingdeMacBook-Air.local>
2015-07-28 23:06:46 +0800

    update to use reveal js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Cache in Learn', 
+  	author: 'Rain Jiang',
+  	description: 'Usage of cache in Learn System'
+  });
 });
 
 module.exports = router;
